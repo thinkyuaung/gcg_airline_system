f0e1740c9c7aae642a6ca80f1ffd3e7a48de8362
Zin Myo Htay <zhtay8859@gmail.com>
2026-07-16 22:20:22 +0630

    repo change

--- MaupinAirlineTicketSystem/src/main/java/com/example/MaupinAirlineTicketSystem/repository/UserRepository.java
+++ MaupinAirlineTicketSystem/src/main/java/com/example/MaupinAirlineTicketSystem/repository/UserRepository.java
@@ -1,11 +1,5 @@
 package com.example.MaupinAirlineTicketSystem.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-import com.example.MaupinAirlineTicketSystem.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
 
 import java.util.List;
 
