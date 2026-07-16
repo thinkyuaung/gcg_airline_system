31eb4777ab72a8ccf5c9dbdc5eeb1241dacf59e2
Wai Yan <waiyan01011996@gmail.com>
2026-07-16 21:27:27 +0630

    Mapping Login and SingUp from controller.java .HomeController is deleted.

--- MaupinAirlineTicketSystem/src/main/java/com/example/MaupinAirlineTicketSystem/controller/Controller.java
+++ MaupinAirlineTicketSystem/src/main/java/com/example/MaupinAirlineTicketSystem/controller/Controller.java
@@ -4,11 +4,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
-@RequestMapping("/airline")
+@RequestMapping("/airline") 
 public class Controller {
 
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
+    
+    
+    @GetMapping("/")
+    public String index() {
+        return "index"; 
+    }
+
+   
+    @GetMapping("/login")
+    public String showLoginPage() {
+        
+        return "Login/login"; 
+    }
+
+       @GetMapping("/about")
+    public String showAboutPage() {
+        return "about";
+    } 
+
+   
+    @GetMapping("/terms")
+    public String showTermsPage() {
+        return "terms"; 
+    }
 }
