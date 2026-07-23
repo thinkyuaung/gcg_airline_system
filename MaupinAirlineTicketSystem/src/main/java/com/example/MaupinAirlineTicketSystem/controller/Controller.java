1a2b7d2f0a496536e25fb5852c8ec54a1240c599
Thin Kyu Aung <thinkyuaung.tka@gmail.com>
2026-07-23 15:22:09 +0630

    fix

--- MaupinAirlineTicketSystem/src/main/java/com/example/MaupinAirlineTicketSystem/controller/Controller.java
+++ MaupinAirlineTicketSystem/src/main/java/com/example/MaupinAirlineTicketSystem/controller/Controller.java
@@ -55,7 +55,7 @@ public class Controller {
 	    /////////////////////////
 	    
 	    model.addAttribute(
-		        "seatClasses",
+		        "seatClasses",seatClassRepository.findAll()
 		    );
 
 	    return "index";
