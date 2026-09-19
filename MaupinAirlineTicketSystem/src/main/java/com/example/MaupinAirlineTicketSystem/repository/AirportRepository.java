bc2049e9ce861fb84ecc1f37864c5e1a9107d592
Stella <thinkyuaung.tka@gmail.com>
2026-09-20 01:09:33 +0630

    Delete MaupinAirlineTicketSystem directory

--- MaupinAirlineTicketSystem/src/main/java/com/example/MaupinAirlineTicketSystem/repository/AirportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MaupinAirlineTicketSystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.MaupinAirlineTicketSystem.entity.Airport;
-@Repository
-public interface AirportRepository extends JpaRepository<Airport, Integer> {
-	
-}
